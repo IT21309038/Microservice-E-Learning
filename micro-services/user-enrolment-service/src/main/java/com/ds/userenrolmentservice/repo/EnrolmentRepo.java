4e0c2ff12cb49e193af7affc3162bffdedae0adf
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2024-05-14 00:28:21 +0530

    updated enrolments

--- micro-services/user-enrolment-service/src/main/java/com/ds/userenrolmentservice/repo/EnrolmentRepo.java
+++ micro-services/user-enrolment-service/src/main/java/com/ds/userenrolmentservice/repo/EnrolmentRepo.java
@@ -3,13 +3,14 @@ package com.ds.userenrolmentservice.repo;
 import com.ds.userenrolmentservice.model.Enrolment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EnrolmentRepo extends MongoRepository<Enrolment, String>{
 
-    Optional<Enrolment> findByUserId(String userId);
+    List<Enrolment> findByUserId(String userId);
 
-    Optional<Enrolment> findByCourseId(String courseId);
+    List<Enrolment> findByCourseId(String courseId);
 
     Optional<Enrolment> findByUserIdAndCourseId(String userId, String courseId);
 
