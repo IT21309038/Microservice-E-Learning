e4810a283b6f9702369f1c50895d1a03c6b069ca
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2025-04-18 17:03:24 +0530

    Update main.tf

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -78,8 +78,8 @@ resource "aws_eks_node_group" "default" {
   subnet_ids      = data.aws_subnets.default.ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
